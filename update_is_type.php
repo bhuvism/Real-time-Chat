dd40392e550702ccaec789cc98057a4c8c38b211
Bhuvan S M <bhuvanbhuvism@gmail.com>
2020-09-30 21:32:26 +0530

    Update update_is_type.php

--- update_is_type.php
+++ update_is_type.php
@@ -1,5 +1,9 @@
 <?php 
-   $conn = new mysqli("localhost","root","75;#)dFys_ei","social");
+  $server = "localhost";
+$mysql_username = "<your_mysql_username>";
+$mysql_password = "<your_mysql_password>";
+$db_name = "<your_db_name>";
+$conn = new mysqli($server,$mysql_username,$mysql_password,$db_name);
    $conn->set_charset("utf8mb4");
    session_start();
    $query = "UPDATE login_details SET is_type='".$_POST['is_type']."' WHERE login_details_id='".$_SESSION['last_id']."'";
